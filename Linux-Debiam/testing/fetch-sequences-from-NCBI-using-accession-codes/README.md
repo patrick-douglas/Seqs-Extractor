4a928ff14590a428acc3f66a4f369233183f01a4
Patrick Douglas <24271907+patrick-douglas@users.noreply.github.com>
2023-04-29 13:33:42 -0300

    sub_dir

---
+++ Linux-Debiam/testing/fetch-sequences-from-NCBI-using-accession-codes/README.md
@@ -0,0 +1,3 @@
+# Fetch-sequences-from-NCBI-using-accession-codes
+## Fetch sequences from NCBI using accession codes
+Usage: ./Fetch-sequences-from-NCBI-using-accession-codes.sh -l taxid11118.acc_lst
---
+++ Linux-Debiam/testing/fetch-sequences-from-NCBI-using-accession-codes/fetch-sequences-from-NCBI-using-accession-codes.sh
@@ -0,0 +1,90 @@
+#!/bin/bash 
+w=$(tput sgr0) 
+r=$(tput setaf 1)
+g=$(tput setaf 2) 
+y=$(tput setaf 3) 
+USAGE="USAGE: $0 -l <ncbi_accessions_list>"
+EXAMPLE_ACC_LST="
+Example:
+NC_048212
+NC_034440
+NC_046954
+NC_046964
+NC_025217
+NC_010437
+NC_014470
+NC_022103
+NC_010646
+NC_038294"
+if [ "$#" -lt "1" ] 
+then 
+    echo -e $USAGE;
+exit 1    
+else 
+    var1=$2;
+fi
+
+while getopts l: option
+do
+case "${option}"
+in
+l) acc_lst=$OPTARG;;
+esac
+done
+
+if [ "$acc_lst" = "" ]; then
+echo "${g}Error, missing arguments:${y} -l | NCBI accession list | <string>"
+echo "${w}$USAGE"
+echo "$EXAMPLE_ACC_LST"
+exit 1
+fi
+num_acc=`cat $acc_lst | wc -l`
+echo "====================================="
+echo "Total number of seqs to fetch: $num_acc"
+echo "====================================="
+echo ""
+rm -rf .download_$acc_lst.sh
+cat $acc_lst | sed -e 's/^/esearch -db nucleotide -query "/' > .download_$acc_lst.sh
+sed -e 's/$/" | efetch -format fasta > /' -i .download_$acc_lst.sh
+sed ':a;N;$!ba;s/\r//g' -i .download_$acc_lst.sh
+cat .download_$acc_lst.sh | awk -F " " '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,"."$5".fa.tmp"}' > .2download_$acc_lst.sh
+chmod +x .2download_$acc_lst.sh
+
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+( ./.2download_$acc_lst.sh ) &
+echo -n "> Fetching seqs from NCBI, please wait... "
+spinner $!
+echo " DONE!"
+cat .*.fa.tmp > $acc_lst.fasta
+rm .2download_$acc_lst.sh .download_$acc_lst.sh 
+rm -rf .*.fa.tmp
+echo "> Generated a single FASTA file: $acc_lst.fasta"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
